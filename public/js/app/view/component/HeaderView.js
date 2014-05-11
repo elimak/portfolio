2163c6e3ae3c319b40d1e9d8129a377b6f1a0894
valerie <valerie@entefy.com>
2014-05-11 12:36:02 -0700

    Fixed header view and the main navigation dynamically

--- public/js/app/view/component/HeaderView.js
+++ public/js/app/view/component/HeaderView.js
@@ -6,8 +6,9 @@
  * To change this template use File | Settings | File Templates.
  */
 define([
-    'hbs!templates/component/HeaderView'
-], function (template) {
+    'hbs!templates/component/HeaderView',
+    'constant/States',
+], function (template, States) {
     var HeaderView = Backbone.View.extend({
 
         navigatorBehaviors: ["IHasStateTransition"],
@@ -21,9 +22,44 @@ define([
             this.render();
         },
 
+        events: {
+            'click .logo_def'       : 'onRequestNavigation' ,
+            'click .logo_retina'    : 'onRequestNavigation' ,
+            'click #menu_home'      : 'onRequestNavigation' ,
+            'click #menu_portfolio' : 'onRequestNavigation' ,
+            'click #menu_resume'    : 'onRequestNavigation' ,
+            'click #menu_contact'   : 'onRequestNavigation'
+        },
+
+        onRequestNavigation: function(e){
+            e.preventDefault();
+            var state = $(e.target).attr('href');
+
+            _.each([$( "#menu_home" ), $( "#menu_portfolio" ), $( "#menu_resume" ), $( "#menu_contact" )],
+                    function(element){
+                        element.removeClass("current-menu-parent");
+                    });
+
+            switch(state){
+                case States.HOME.getLastSegment():
+                    $( "#menu_home" ).addClass( "current-menu-parent" );
+                    break;
+                case States.PORTFOLIO.getLastSegment():
+                    $( "#menu_portfolio" ).addClass( "current-menu-parent" );
+                    break;
+                case States.RESUME.getLastSegment():
+                    $( "#menu_resume" ).addClass( "current-menu-parent" );
+                    break;
+                case States.CONTACT.getLastSegment():
+                    $( "#menu_contact" ).addClass( "current-menu-parent" );
+                    break;
+            }
+
+            this.njs.request(state);
+        },
+
         render: function() {
             this.$el.html(template({}));
-
             return this;
         },
 
